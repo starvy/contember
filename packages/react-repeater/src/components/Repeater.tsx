ba7f6984fa46d5ef13cc102c814866d35285e686
jonasnobile <mesons_rates0p@icloud.com>
2025-02-27 09:41:14 +0100

    feat(react-repeater): determinate orderBy from sortableBy prop

--- packages/react-repeater/src/components/Repeater.tsx
+++ packages/react-repeater/src/components/Repeater.tsx
@@ -1,21 +1,24 @@
-import React, { useEffect, useMemo } from 'react'
-import { verifySortableProp } from '../internal/verifySortableProp'
+import type { SugaredOrderBy } from '@contember/binding-common'
+import { ReactNode, useEffect, useMemo } from 'react'
 import { useCreateRepeaterMethods } from '../internal/useCreateRepeaterMethods'
 import { RepeaterEntityListAccessorContext, RepeaterMethodsContext, RepeaterSortedEntitiesContext } from '../contexts'
 import { Component, EntityListSubTree, HasMany, repairEntitiesOrder, sortEntities, SugaredField, useEntityList, useEntityListSubTree, useEnvironment } from '@contember/react-binding'
 import { EntityListAccessor, QueryLanguage, SugaredQualifiedEntityList, SugaredRelativeEntityList, SugaredRelativeSingleField } from '@contember/react-binding'
+import { verifySortableProp } from '../internal/verifySortableProp'
 
 export type RepeaterRelativeProps =
 	& SugaredRelativeEntityList
 	& {
-		children?: React.ReactNode
+		children?: ReactNode
+		orderBy?: SugaredOrderBy
 		sortableBy?: SugaredRelativeSingleField['field']
 	}
 
 export type RepeaterQualifiedProps =
 	& SugaredQualifiedEntityList
 	& {
-		children?: React.ReactNode
+		children?: ReactNode
+		orderBy?: SugaredOrderBy
 		sortableBy?: SugaredRelativeSingleField['field']
 	}
 
@@ -37,10 +40,18 @@ export const Repeater = Component<RepeaterProps>(props => {
 	return <RepeaterRelative {...props} />
 })
 
+const useSortableBy = (sortableBy?: SugaredRelativeSingleField['field']) => {
+	const environment = useEnvironment()
+	return useMemo(() => sortableBy ? QueryLanguage.desugarRelativeSingleField(sortableBy, environment) : undefined, [environment, sortableBy])
+}
 
 const RepeaterRelative = Component(
 	(props: RepeaterRelativeProps) => {
-		const entityList = useEntityList(props)
+		const desugarSortableBy = useSortableBy(props.sortableBy)
+		const entityList = useEntityList({
+			...props,
+			orderBy: props.orderBy ?? desugarSortableBy?.field,
+		})
 
 		return (
 			<RepeaterInner accessor={entityList} sortableBy={props.sortableBy}>
@@ -48,33 +59,49 @@ const RepeaterRelative = Component(
 			</RepeaterInner>
 		)
 	},
-	(props, environment) => (
-		<HasMany {...props}>
-			{props.children}
-			{props.sortableBy && <SugaredField field={props.sortableBy} isNonbearing />}
-		</HasMany>
-	),
+	(props, environment) => {
+		const desugarSortableBy = props.sortableBy
+			? QueryLanguage.desugarRelativeSingleField(props.sortableBy, environment)
+			: undefined
+
+		return (
+			<HasMany {...props} orderBy={props.orderBy ?? desugarSortableBy?.field}>
+				{props.children}
+				{props.sortableBy && <SugaredField field={props.sortableBy} isNonbearing />}
+			</HasMany>
+		)
+	},
 	'Repeater',
 )
 
 const RepeaterQualified = Component(
 	(props: RepeaterQualifiedProps) => {
-		const entityList = useEntityListSubTree(props)
+		const desugarSortableBy = useSortableBy(props.sortableBy)
+		const entityList = useEntityListSubTree({
+			...props,
+			orderBy: props.orderBy ?? desugarSortableBy?.field,
+		})
+
 		return (
 			<RepeaterInner accessor={entityList} sortableBy={props.sortableBy}>
 				{props.children}
 			</RepeaterInner>
 		)
 	},
-	(props, environment) => (
-		<EntityListSubTree {...props}>
-			{props.children}
-			{props.sortableBy && <SugaredField field={props.sortableBy} isNonbearing />}
-		</EntityListSubTree>
-	),
+	(props, environment) => {
+		const desugarSortableBy = props.sortableBy
+			? QueryLanguage.desugarRelativeSingleField(props.sortableBy, environment)
+			: undefined
+
+		return (
+			<EntityListSubTree {...props} orderBy={props.orderBy ?? desugarSortableBy?.field}>
+				{props.children}
+				{props.sortableBy && <SugaredField field={props.sortableBy} isNonbearing />}
+			</EntityListSubTree>
+		)
+	},
 	'Repeater',
 )
-
 
 interface RepeaterInnerProps {
 	accessor: EntityListAccessor
@@ -83,11 +110,8 @@ interface RepeaterInnerProps {
 }
 
 const RepeaterInner = ({ sortableBy, accessor, children }: RepeaterInnerProps) => {
-	const environment = useEnvironment()
-	const desugaredSortableByField = useMemo(() => sortableBy ? QueryLanguage.desugarRelativeSingleField(sortableBy, environment) : undefined, [environment, sortableBy])
-	const sortedEntities = useMemo(() => {
-		return sortEntities(Array.from(accessor), desugaredSortableByField)
-	}, [desugaredSortableByField, accessor])
+	const desugaredSortableByField = useSortableBy(sortableBy)
+	const sortedEntities = useMemo(() => sortEntities(Array.from(accessor), desugaredSortableByField), [desugaredSortableByField, accessor])
 
 	useEffect(() => {
 		if (!desugaredSortableByField) {
